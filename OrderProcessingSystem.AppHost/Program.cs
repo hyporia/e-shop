7052ac8228661c494e1a08088bc2d8def0a67f39
Maxim Galimov <maximgalimov96@gmail.com>
2024-09-04 22:41:27 +0200

    add slnx file, change solution structure

--- OrderProcessingSystem.AppHost/Program.cs
+++ OrderProcessingSystem.AppHost/Program.cs
@@ -1,12 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var messaging = builder.AddRabbitMQ("messaging")
-    .WithDockerfile("");
+var messaging = builder.AddRabbitMQ("messaging");
 
 builder.AddProject<Projects.UserService>("userservice")
     .WithReference(messaging);
 
-builder.AddProject<Projects.EmailService>("emailservice")
+builder.AddProject<Projects.NotificationService>("emailservice")
     .WithReference(messaging);
 
 builder.Build().Run();
