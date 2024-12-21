77f867ce86e6719bcbf4f2970c9557329ea5debc
Maxim Galimov <maximgalimov96@gmail.com>
2024-12-21 16:23:16 +0100

    #3 Fix imports on the client side

--- src/Clients/onlineshop/src/components/Home/Home.tsx
+++ src/Clients/onlineshop/src/components/Home/Home.tsx
@@ -1,17 +1,19 @@
-import React from "react";
 import "./Home.css";
 
 const Home = (): JSX.Element => {
-  return (
-    <div className="home">
-      <header className="home-header">
-        <h1>Welcome to the Online Shop</h1>
-      </header>
-      <main className="home-content">
-        <p>Discover our wide range of products and enjoy shopping with us!</p>
-      </main>
-    </div>
-  );
+    return (
+        <div className="home">
+            <header className="home-header">
+                <h1>Welcome to the Online Shop</h1>
+            </header>
+            <main className="home-content">
+                <p>
+                    Discover our wide range of products and enjoy shopping with
+                    us!
+                </p>
+            </main>
+        </div>
+    );
 };
 
 export default Home;
