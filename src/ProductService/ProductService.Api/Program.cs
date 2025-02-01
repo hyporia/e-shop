b2b609172a082dc220ce1de3fab01e7c77d8c46d
Maxim Galimov <maximgalimov96@gmail.com>
2025-02-01 15:12:37 +0100

    #5 remove Swagger configuration from ProductService, OrderService, and ShippingService

--- src/ProductService/ProductService.Api/Program.cs
+++ src/ProductService/ProductService.Api/Program.cs
@@ -9,18 +9,12 @@ builder.AddServiceDefaults();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
